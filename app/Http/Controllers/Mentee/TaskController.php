a406acb39993cf153365f61c6d9c8949eae40369
mulyatma <mulyaatmaja604@gmail.com>
2025-01-29 01:15:28 +0700

    remove return JSON

--- app/Http/Controllers/Mentee/TaskController.php
+++ app/Http/Controllers/Mentee/TaskController.php
@@ -59,19 +59,8 @@ class TaskController extends Controller
 
             $gradingStatus = 'Not graded';
 
-            // return response()->json([
-            //     $task,
-            //     $opened,
-            //     $deadline,
-            //     $submissionStatus,
-            //     $gradingStatus,
-            //     $timeRemaining,
-            //     $lastModified
-            // ]);
-
             return view('mentee.task', compact('task', 'opened', 'deadline', 'submissionStatus', 'gradingStatus', 'timeRemaining', 'lastModified', 'file'));
         } catch (\Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 500);
             return redirect()->back()->with('error', $e->getMessage());
         }
     }
