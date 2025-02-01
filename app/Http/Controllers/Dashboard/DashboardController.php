826ea97950d5f70597fdfc3500a53b9620ffbcb8
mulyatma <mulyaatmaja604@gmail.com>
2025-02-01 20:32:40 +0700

    delete coment

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -16,24 +16,20 @@ class DashboardController extends Controller
         $today = Carbon::now();
         $nextWeek = Carbon::now()->addDays(7);
 
-        // Ambil daftar course yang diikuti user
         $userCourses = DB::table('course_user')
             ->where('user_id', $userId)
             ->pluck('course_id');
 
-        // Ambil daftar module_id yang terkait dengan course yang diikuti user
         $userModules = DB::table('modules')
             ->whereIn('course_id', $userCourses)
             ->pluck('module_id');
 
-        // Ambil tasks berdasarkan module yang terkait dengan course yang diikuti user
         $tasks = DB::table('tasks')
             ->whereIn('module_id', $userModules)
             ->whereBetween('deadline', [$today, $nextWeek])
             ->select('title', 'deadline')
             ->get();
 
-        // Ambil attendances berdasarkan module yang terkait dengan course yang diikuti user
         $attendances = DB::table('attendances')
             ->whereIn('module_id', $userModules)
             ->whereBetween('attendance_open', [$today, $nextWeek])
@@ -56,8 +52,6 @@ class DashboardController extends Controller
                 'end' => $attendance->end,
             ];
         }
-
-        // return response()->json($events);
 
         return view('mentee.dashboard', compact('events'));
     }
