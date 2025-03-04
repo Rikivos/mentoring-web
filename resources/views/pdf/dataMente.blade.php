46591fa4a5bd5b7c7e265a9c95b877352999687b
mulyatma <mulyaatmaja604@gmail.com>
2025-03-05 06:12:20 +0700

    add headaer pdf

--- resources/views/pdf/dataMente.blade.php
+++ resources/views/pdf/dataMente.blade.php
@@ -8,6 +8,33 @@
             font-family: Arial, sans-serif;
         }
 
+        h1,
+        h2 {
+            font-size: medium;
+            margin: 5px;
+            text-align: center;
+        }
+
+        .header-table {
+            width: 100%;
+            text-align: center;
+            margin-bottom: 5px;
+        }
+
+        .header-table td {
+            vertical-align: middle;
+        }
+
+        .header-table img {
+            width: 80px;
+            height: 80px;
+        }
+
+        hr {
+            border: 2px solid black;
+            margin: 5px 0;
+        }
+
         table {
             width: 100%;
             border-collapse: collapse;
@@ -27,6 +54,22 @@
 </head>
 
 <body>
+    <table class="header-table">
+        <tr>
+            <td style="width: 20%; text-align: left;">
+                <img src="images/LPPI.png" alt="Logo LPPI">
+            </td>
+            <td style="width: 60%;">
+                <h1>LEMBAGA PENGKAJIAN DAN PENGAMALAN ISLAM</h1>
+                <h2>UNIVERSITAS MUHAMMADIYAH PURWOKERTO</h2>
+            </td>
+            <td style="width: 20%; text-align: right;">
+                <img src="images/UMPLogo.png" alt="Logo UMP">
+            </td>
+        </tr>
+    </table>
+    <hr>
+
     <h1>Laporan Kelas: {{ $course_name }}</h1>
     <p><strong>Mentor:</strong> {{ $mentor_name }}</p>
     <p><strong>Jumlah Peserta:</strong> {{ count($participants) }}</p>
