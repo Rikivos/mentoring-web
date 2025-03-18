6923301f2260a4e98d699e8f24be3d3fae89b193
mulyatma <mulyaatmaja604@gmail.com>
2025-03-18 21:50:48 +0700

    chnge tabel at admin class and change footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,5 @@
 @if(request()->is('login') === false)
-    <footer class="bg-blue-700 text-center py-4 text-sm text-white">
-        © Copyright - UMP | Theme by RWMCode
-    </footer>
+<footer class="bg-blue-700 text-center py-4 text-sm text-white">
+    © Copyright - UMP
+</footer>
 @endif
